3ae00a8bc60e022e2167e3139b3a8314ce773e94
Kevin Martzolf <kmart914@gmail.com>
2017-07-08 22:18:21 -0700

    few changes

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -7,27 +7,54 @@ var losses = 0;
 var wordChosen =
   computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
+
 var guessesLeft = 10;
 
 var guesses = [];
 
+var res = wordChosen.split("");
+
+var currentWord = [];
 
 var reset = function(){
   guessesLeft = 10
   guesses =[];
-  wordChosen =
+  wordChosen = [];
   computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
+for( var i = 0; i < wordChosen.length; i++){
+  currentWord += "_ ";
+}
+
+
+
+  console.log(res);
   console.log(wordChosen);
 
 document.onkeyup = function(event){
 
 guessesLeft--;
-var userGuess = "" +
-event.key.toLowerCase();
+var userGuess = "" + event.key.toLowerCase();
 
 guesses.push(userGuess);
 console.log(guesses);
+
+if (event.key === res){
+  document.querySelector('#currentWord').innerHTML = currentWord.join("");
+};
+
+
+
+
+
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+document.getElementById("guesses").innerHTML = guesses;
+document.getElementById("currentWord").innerHTML = currentWord;
+
+
+
 
 }
