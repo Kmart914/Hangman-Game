23da07ec2b87749c632e3d93bda545ffd3199d20
unknown <kmart914@gmail.com>
2017-07-08 23:01:17 -0700

    added notes

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,21 +1,27 @@
+//Array to hold all of the different words that will be used in the game.
 var computerChoice =["football", "baseball", "soccer", "golf", "cricket", "water polo", "hockey", "volleyball", "cricket", "bowling", "ping pong", "tennis", "lacrosse", "surfing", "softball", "rugby", "curling"];
-
+// Total Wins
 var wins = 0;
 
+// Total losses
 var losses = 0;
 
+//Computer will select a random word from the computerChoice array and will be stored here.
 var wordChosen =
   computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
-
+//Amount of guesses that the person has left.
 var guessesLeft = 10;
-
+// Variable to show which guesses they have already made.
 var guesses = [];
 
+// Variable to split the array into single letters.
 var res = wordChosen.split("");
 
+// variable that will hold the underscores for the length of the word
 var currentWord = [];
 
+// Reset function to run after choices are made
 var reset = function(){
   guessesLeft = 10
   guesses =[];
@@ -23,19 +29,25 @@ var reset = function(){
   computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
+// This for loop will look at the length of the word that the computer selected and add an underscore for each letter.
 for( var i = 0; i < wordChosen.length; i++){
   currentWord += "_ ";
 }
 
 
-
-  console.log(res);
-  console.log(wordChosen);
-
+//Calling a function for anytime that someone hits a key.
 document.onkeyup = function(event){
 
+//every time they hit a key the guesses will go down by one
 guessesLeft--;
 var userGuess = "" + event.key.toLowerCase();
+
+if (guessesLeft === 0){
+  losses++;
+  reset();
+  alert("Try Again!");
+}
+
 
 guesses.push(userGuess);
 console.log(guesses);
@@ -43,6 +55,8 @@ console.log(guesses);
 if (event.key === res){
   document.querySelector('#currentWord').innerHTML = currentWord.join("");
 };
+
+
 
 
 
@@ -58,3 +72,6 @@ document.getElementById("currentWord").innerHTML = currentWord;
 
 
 }
+
+console.log(res);
+console.log(wordChosen);
