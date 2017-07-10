2259693718c94c0cf59c8911fcd4b01ff8ae51bd
unknown <kmart914@gmail.com>
2017-07-09 22:32:46 -0700

    added changes that I worked on with Andrew

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -9,69 +9,70 @@ var losses = 0;
 //Computer will select a random word from the computerChoice array and will be stored here.
 var wordChosen =
   computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
+// letters of chosen word in array
+var res = [];
 //Amount of guesses that the person has left.
 var guessesLeft = 10;
 // Variable to show which guesses they have already made.
 var guesses = [];
 
-// Variable to split the array into single letters.
-var res = wordChosen.split("");
+
 
 // variable that will hold the underscores for the length of the word
-var currentWord = [];
+var underscores = [];
 
-// Reset function to run after choices are made
+// Reset function to run after certain conditions are met.
 var reset = function(){
-  guessesLeft = 10
-  guesses =[];
-  wordChosen = [];
-  computerChoice[Math.floor(Math.random() * computerChoice.length)];
-}
 
-// This for loop will look at the length of the word that the computer selected and add an underscore for each letter.
-for( var i = 0; i < wordChosen.length; i++){
-  currentWord += "_ ";
+  res = wordChosen.split("");
+  guessesLeft = 10
+  guesses = [];
+  wordChosen = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+  for( var i = 0; i < wordChosen.length; i++){
+  underscores.push("_");
+  }
+document.getElementById("currentWord").innerHTML = underscores.join(" ");
+  document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  document.getElementById("guesses").innerHTML = guesses;
 }
+// This for loop will look at the length of the word that the computer selected and add an underscore for each letter.
+
+
 
 
 //Calling a function for anytime that someone hits a key.
 document.onkeyup = function(event){
+  // store the key  the user pressed in a variable
+  var userGuess = event.key.toLowerCase();
+  // add to guesses array
+  guesses.push(userGuess);
+  //every time they hit a key the guesses will go down by one
+  guessesLeft--;
 
-//every time they hit a key the guesses will go down by one
-guessesLeft--;
-var userGuess = "" + event.key.toLowerCase();
+  for( var i = 0; i < res.length; i++){
+  }
 
-if (guessesLeft === 0){
-  losses++;
-  reset();
-  alert("Try Again!");
+  /*
+       res =         ["c", "r", "i", "c", "k", "e", "t"]
+       underscores = ["_", "_", "_", "_", "_", "_", "_"]
+       index:          0    1    2    3    4    5    6
+      Step 1: loop through the res array and check if the userGuess is equal to a letter.
+      Step 2: if they are equal then replace the underscores array with that value, in the correct index.
+  */
+
+  //This will add one loss, reset the game, and alert them to try again if they use all 10 guesses.
+  if (guessesLeft === 0){
+    losses++;
+    reset();
+    alert("Try Again!");
+  }
+
+  document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  document.getElementById("guesses").innerHTML = guesses.join(" ");
 }
 
-
-guesses.push(userGuess);
+reset();
 console.log(guesses);
-
-if (event.key === res){
-  document.querySelector('#currentWord').innerHTML = currentWord.join("");
-};
-
-
-
-
-
-
-
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = losses;
-document.getElementById("guessesLeft").innerHTML = guessesLeft;
-document.getElementById("guesses").innerHTML = guesses;
-document.getElementById("currentWord").innerHTML = currentWord;
-
-
-
-
-}
-
 console.log(res);
-console.log(wordChosen);
+console.log(underscores);
+console.log(res.length);
